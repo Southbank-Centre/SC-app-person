cb3c2b9aebfe4df66bbc8d3b2293be1a385cad3e
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:10:19 +0000

    initial commit

---
+++ app/SC-app-person.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-person
+ * @description
+ *
+ * Provides the app with the ability to display person content and features
+ */
+angular
+  .module('SC-app-person');
